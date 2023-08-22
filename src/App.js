61a1697205bfb6a45648b205b384ebc6ce51fb0b
HarshalMesare <harshalmesare89640@gmail.com>
2023-08-22 19:18:00 +0530

    login-page creation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Home from './scss/Home';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import LoginPage from './scss/components/Login-Page/login-page';
 
-function App() {
+export default function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React hello world nsdnddhdhhddhddh
-        </a>
-      </header>
+        {/* <Switch> */}
+        <LoginPage/>
+        <Home/>
+          {/* <Route path="/login" component={LoginPage} />
+          <Route path="/home" component={Home} /> */}
+        {/* </Switch> */}
+      <Home/>
     </div>
-  );
+     </Router>
+  )
 }
-
-export default App;
---
+++ src/scss/components/Login-Page/login-page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function LoginPage() {
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <h2>Login Here</h2>
+        <button className="login-button">Login</button>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
